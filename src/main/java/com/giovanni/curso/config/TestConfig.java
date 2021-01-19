36dfa397242749e4f32b1654643c2fdea4cb1898
Giovanni <giovanniovecchi@gmail.com>
2021-01-18 21:55:53 -0300

    Aula 10 - Associação da tabela OrderItem com a Tabela Order

--- src/main/java/com/giovanni/curso/config/TestConfig.java
+++ src/main/java/com/giovanni/curso/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.giovanni.curso.entities.Category;
 import com.giovanni.curso.entities.Order;
+import com.giovanni.curso.entities.OrderItem;
 import com.giovanni.curso.entities.Product;
 import com.giovanni.curso.entities.User;
 import com.giovanni.curso.entities.enums.OrderStatus;
 import com.giovanni.curso.repositories.CategoryRepository;
+import com.giovanni.curso.repositories.OrderItemRepository;
 import com.giovanni.curso.repositories.OrderRepository;
 import com.giovanni.curso.repositories.ProductRepository;
 import com.giovanni.curso.repositories.UserRepository;
@@ -35,6 +37,9 @@ public class TestConfig implements CommandLineRunner {
 	@Autowired
 	private ProductRepository productRepository;
 
+	@Autowired 
+	private OrderItemRepository orderItemRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
 
@@ -68,7 +73,13 @@ public class TestConfig implements CommandLineRunner {
 		productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));		
 		userRepository.saveAll(Arrays.asList(u1,u2));		
 		orderRepository.saveAll(Arrays.asList(o1,o2,o3));
+		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 				
+		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
 	}
 }
 
