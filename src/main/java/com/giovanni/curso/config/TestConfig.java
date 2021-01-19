e9c6f19ca95a580ea4535bce972c6981e40a8b26
Giovanni <giovanniovecchi@gmail.com>
2021-01-18 21:17:55 -0300

    Aula 9 - ManyToMany and JoinTable

--- src/main/java/com/giovanni/curso/config/TestConfig.java
+++ src/main/java/com/giovanni/curso/config/TestConfig.java
@@ -53,12 +53,22 @@ public class TestConfig implements CommandLineRunner {
 		Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
 		Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
 		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
-		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, ""); 
+		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
 		
-		userRepository.saveAll(Arrays.asList(u1,u2));		
-		orderRepository.saveAll(Arrays.asList(o1,o2,o3));
 		categoryRepository.saveAll(Arrays.asList(cat1,cat2,cat3));
 		productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
+		
+		p1.getCategories().add(cat2);
+		p2.getCategories().add(cat1);
+		p2.getCategories().add(cat3);
+		p3.getCategories().add(cat3);
+		p4.getCategories().add(cat3);
+		p5.getCategories().add(cat2);	
+		
+		productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));		
+		userRepository.saveAll(Arrays.asList(u1,u2));		
+		orderRepository.saveAll(Arrays.asList(o1,o2,o3));
+				
 	}
 }
 
